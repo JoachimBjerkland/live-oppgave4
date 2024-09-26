05bf48547fa222b13ffdd12457b142e3f830244b
Joachim Bjerkland <Bjerkland2011@hotmail.no>
2024-09-26 11:13:03 +0200

    Update

--- frontend/components/Grid.tsx
+++ frontend/components/Grid.tsx
@@ -4,25 +4,25 @@ import type { Student as StudentProp } from "./types";
 import AddStudentForm from "./AddStudentForm";
 
 type GridProps = {
-    students: StudentProp[];
+	students: StudentProp[];
 };
 
 export default function Grid(props: GridProps) {
-    // props.students ?? []
-    const [students, setStudents] = useState<StudentProp[]>(props.students ?? []);
+	// props.students ?? []
+	const [students, setStudents] = useState<StudentProp[]>(props.students ?? []);
 
-    const onAddStudent = (student: { name: string }) => {
-        setStudents((prev) => [...prev, { id: crypto.randomUUID(), ...student }]);
-    };
+	const onAddStudent = (student: { name: string }) => {
+		setStudents((prev) => [...prev, { id: crypto.randomUUID(), ...student }]);
+	};
 
-    return (
-        <section>
-            <article className="grid">
-                {students.map((student) => (
-                    <Student key={student.id} name={student.name} id={student.id} />
-                ))}
-            </article>
-            <AddStudentForm onAddStudent={onAddStudent} />
-        </section>
-    );
+	return (
+		<section>
+			<article className="grid">
+				{students.map((student) => (
+					<Student key={student.id} name={student.name} id={student.id} />
+				))}
+			</article>
+			<AddStudentForm onAddStudent={onAddStudent} />
+		</section>
+	);
 }
